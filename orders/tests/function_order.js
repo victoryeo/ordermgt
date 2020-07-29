1917479f219ac2f72183688a6a44763792f8a4a1
victor yeo <victor.yeo@gmail.com>
2020-07-29 17:15:48 +0800

    remove type form from chai request

--- orders/tests/function_order.js
+++ orders/tests/function_order.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai')
 let chaiHttp = require('chai-http')
 let server = require('../index')
@@ -9,12 +11,12 @@ describe("test order", () => {
   it("should call order api", (done) => {
     chai.request(server)
         .post('/api/order')
-        .type('form')
         .send({
           'name': 'tea',
           'amount': 1
         })
         .end((err, res) => {
+            console.log('err ' + err)
             res.should.have.status(200)
             done()
         })

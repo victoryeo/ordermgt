fb63645de0ed050ca13665ab2e17afc23ec089a4
Victor Yeo <victoryeo1974@gmail.com>
2020-07-30 09:27:49 +0800

    test rabbitmq server

--- payments/tests/function_payment.js
+++ payments/tests/function_payment.js
@@ -2,21 +2,21 @@ const chai = require('chai')
 let chaiHttp = require('chai-http')
 let server = require('../index')
 let should = chai.should()
+const amqplib = require('amqplib-mocks')
+const { assert } = require('chai')
 
 chai.use(chaiHttp);
 
-describe("test payment", () => {
-  it("should call payments api", (done) => {
-    chai.request(server)
-        .post('/api/payments')
-        .type('form')
-        .send({
-          'name': 'tea',
-          'amount': 1
-        })
-        .end((err, res) => {
-            res.should.have.status(200)
-            done()
-        })
+describe("test rabbitmq server", (done) => {
+  let connection, channal
+  let queue = 'test'
+  
+  before(async() => {
+    connection = await amqplib.connect("amqp://localhost")
+    channel = await connection.createChannel()
+    await channel.assertQueue(queue)
+  })
+  it("should create the channel", (done) => {
+    assert.isObject(channel)
   })
 })

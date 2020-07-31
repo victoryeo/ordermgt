a318c32687e11427fbe4923bbdbdb4dfa2aabf73
Victor Yeo <victoryeo1974@gmail.com>
2020-07-31 20:59:35 +0800

    add more unit test cases

--- orders/tests/function_order.js
+++ orders/tests/function_order.js
@@ -1,19 +1,45 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test'
 
 const chai = require('chai')
-let chaiHttp = require('chai-http')
-let server = require('../index')
-let should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../index')
+const should = chai.should()
+const testMongoUrl = process.env.MONGO_HOST
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
 describe("test order", () => {
+  it("should assign mongodb url", (done) => {
+    process.env.MONGO_URL = testMongoUrl
+    done()
+  })
   it("should call order api", (done) => {
     chai.request(server)
         .post('/api/order')
         .send({
           'name': 'tea',
           'amount': 1
+        })
+        .end((err, res) => {
+            console.log('err ' + err)
+            res.should.have.status(200)
+            done()
+        })
+  })
+  it("should get order status", (done) => {
+    chai.request(server)
+        .get('/api/orderstatus/tea')
+        .end((err, res) => {
+            console.log('err ' + err)
+            res.should.have.status(200)
+            done()
+        })
+  })
+  it("should call order cancel api", (done) => {
+    chai.request(server)
+        .post('/api/ordercancel')
+        .send({
+          'name': 'tea',
         })
         .end((err, res) => {
             console.log('err ' + err)

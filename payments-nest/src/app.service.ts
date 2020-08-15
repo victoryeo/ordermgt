e60cd2c1cd7006391768d3bdcaf744fa24ef32a6
Victor Yeo <victoryeo1974@gmail.com>
2020-08-15 10:08:07 +0800

    add order result function

--- payments-nest/src/app.service.ts
+++ payments-nest/src/app.service.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getOrderResult(): string {
+    let result: number = Math.random() * (10 - 1) + 1
+    console.log(result)
+    // send status to order
+    if (result > 5 && result <= 10)
+      return "confirmed";
+    else
+      return "declined";
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
